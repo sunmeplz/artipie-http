1ebadce52fe3f3cf476f509cc8bb9800b3852c39
Kirill <g4s8.public@gmail.com>
2020-02-20 15:46:22 +0300

    #32 - removed implementation

--- src/main/java/com/artipie/http/rq/RqMultipart.java
+++ src/main/java/com/artipie/http/rq/RqMultipart.java
@@ -24,17 +24,10 @@
 
 package com.artipie.http.rq;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.util.concurrent.Flow;
-import java.util.concurrent.Flow.Subscriber;
-import java.util.concurrent.Flow.Subscription;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.cactoos.Text;
-import org.cactoos.scalar.Unchecked;
 
 /**
  * Multipart request.
@@ -42,17 +35,18 @@ import org.cactoos.scalar.Unchecked;
  * <a href="https://www.w3.org/Protocols/rfc1341/7_2_Multipart.html">rfc1341</a>
  * spec.
  * @since 0.4
+ * @todo #32:30min Implement this class.
+ *  It should read the stream of body chunks and convert it to stream of
+ *  request parts. When we detect new request part it should immediately
+ *  emmit new part object.
  */
 public final class RqMultipart implements Flow.Publisher<RqPart> {
 
     /**
-     * Default buffer size.
-     */
-    private static final int BUF_SIZE = 1024 * 8;
-
-    /**
      * Multipart boundary header.
+     * @checkstyle ConstantUsageCheck (500 lines)
      */
+    @SuppressWarnings("PMD.UnusedPrivateField")
     private static final Pattern BOUNDARY = Pattern.compile(".*[^a-z]boundary=([^;]+).*");
 
     /**
@@ -72,6 +66,9 @@ public final class RqMultipart implements Flow.Publisher<RqPart> {
      * Ctor.
      * @param header Content type header value
      * @param body Publisher
+     * @todo #32:30min Add secondary constructor RqMultipart(headers, body)
+     *  to convert content type header to Text right in the constructor.
+     *  Add a unit test to verify this case.
      */
     public RqMultipart(final Text header, final Flow.Publisher<ByteBuffer> body) {
         this.header = header;
@@ -80,126 +77,11 @@ public final class RqMultipart implements Flow.Publisher<RqPart> {
 
     @Override
     public void subscribe(final Flow.Subscriber<? super RqPart> sub) {
-        final Matcher matcher =
-            BOUNDARY.matcher(new Unchecked<>(this.header::asString).value());
-        if (!matcher.matches()) {
-            sub.onSubscribe(
-                new Subscription() {
-                    @Override
-                    public void request(final long req) {
-                        sub.onError(
-                            new IllegalStateException(
-                                "invalid boundary for content type header"
-                            )
-                        );
-                    }
-
-                    @Override
-                    public void cancel() {
-                        // @checkstyle MethodBodyCommentsCheck (1 line)
-                        // nothing to do
-                    }
-                }
-            );
-            return;
-        }
-        final byte[] boundary = String.format("--%s", matcher.group(1))
-            .getBytes(StandardCharsets.UTF_8);
-        final Flow.Processor<ByteBuffer, RqPart> proc = new RqMultipart.Sub(boundary);
-        this.body.subscribe(proc);
-        proc.subscribe(sub);
+        throw new IllegalStateException("not implemented");
     }
 
-    /**
-     * Flow processor to convert multipart chunks to request parts.
-     * @since 0.4
-     */
-    private static final class Sub implements Flow.Processor<ByteBuffer, RqPart> {
-
-        /**
-         * Local state for uncompleted chunk.
-         */
-        private final ByteBuffer state;
-
-        /**
-         * Multipart processor.
-         */
-        private final Multiparts parts;
-
-        /**
-         * Flow subscriber.
-         */
-        private Subscriber<? super RqPart> subscriber;
-
-        /**
-         * Flow subscription.
-         */
-        private Subscription subscription;
-
-        /**
-         * New processor with fixed boundary.
-         * @param boundary Boundary bytes
-         */
-        Sub(final byte[] boundary) {
-            this.parts = new Multiparts(ByteBuffer.wrap(boundary));
-            this.state = ByteBuffer.allocate(RqMultipart.BUF_SIZE);
-        }
-
-        @Override
-        public void onSubscribe(final Subscription sub) {
-            this.subscription = sub;
-            this.request();
-        }
-
-        @Override
-        public void onNext(final ByteBuffer item) {
-            this.state.put(item).flip();
-            try {
-                while (this.parts.push(this.state)) {
-                    this.state.compact().flip();
-                }
-            } catch (final IOException err) {
-                this.subscriber.onError(err);
-                return;
-            }
-            this.state.compact();
-            for (final Path part : this.parts.popAll()) {
-                this.subscriber.onNext(new RqPartFromFile(part));
-            }
-            this.subscription.request(1L);
-        }
-
-        @Override
-        public void onError(final Throwable err) {
-            this.subscriber.onError(err);
-        }
-
-        @Override
-        public void onComplete() {
-            if (this.state.hasRemaining()) {
-                this.subscriber.onError(
-                    new IllegalStateException(
-                        "Subscriber completed at the middle of part receiving"
-                    )
-                );
-            } else {
-                this.subscriber.onComplete();
-            }
-        }
-
-        @Override
-        public void subscribe(final Subscriber<? super RqPart> sub) {
-            this.subscriber = sub;
-            this.request();
-        }
-
-        /**
-         * Request initial item.
-         */
-        private void request() {
-            if (this.subscriber != null && this.subscription != null) {
-                this.subscription.request(1L);
-            }
-        }
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.header, this.body);
     }
 }
