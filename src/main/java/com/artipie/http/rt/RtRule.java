dc49146270eca6964dbbed14a0bc68ff627d44c3
guseyn <guseynism@gmail.com>
2020-03-17 11:27:26 +0300

    fix lint

--- src/main/java/com/artipie/http/rt/RtRule.java
+++ src/main/java/com/artipie/http/rt/RtRule.java
@@ -43,7 +43,7 @@ public interface RtRule {
     /**
      * Fallback RtRule.
      */
-    RtRule FALLBACK = ((line, headers) -> true);
+    RtRule FALLBACK = (line, headers) -> true;
 
     /**
      * Apply this rule to request.
