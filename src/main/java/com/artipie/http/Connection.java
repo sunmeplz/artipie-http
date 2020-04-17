ef933d3b6781a65ad4428d5b6d509807603e73f1
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-04-17 12:22:42 +0300

    #122 - Changes by review

--- src/main/java/com/artipie/http/Connection.java
+++ src/main/java/com/artipie/http/Connection.java
@@ -25,6 +25,7 @@ package com.artipie.http;
 
 import com.artipie.http.rs.RsStatus;
 import java.nio.ByteBuffer;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 import org.reactivestreams.Publisher;
 
@@ -42,4 +43,21 @@ public interface Connection {
      * @return Completion stage for accepting HTTP response.
      */
     CompletionStage<Void> accept(RsStatus status, Headers headers, Publisher<ByteBuffer> body);
+
+    /**
+     * Respond on connection.
+     * @param status The http status code.
+     * @param headers The http response headers.
+     * @param body The http response body.
+     * @return Completion stage for accepting HTTP response.
+     * @deprecated Use {@link Connection#accept(RsStatus, Headers, Publisher)}.
+     */
+    @Deprecated
+    default CompletionStage<Void> accept(
+        RsStatus status,
+        Iterable<Map.Entry<String, String>> headers,
+        Publisher<ByteBuffer> body
+    ) {
+        return this.accept(status, new Headers.From(headers), body);
+    }
 }
