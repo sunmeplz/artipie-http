10522fea7158bb74c24114c6ab8e31103a3d9711
olenagerasimova <olena.gerasimova@gmail.com>
2020-04-22 15:55:20 +0300

    #132 CR

--- src/test/java/com/artipie/http/slice/SliceWithHeadersTest.java
+++ src/test/java/com/artipie/http/slice/SliceWithHeadersTest.java
@@ -25,9 +25,10 @@ package com.artipie.http.slice;
 
 import com.artipie.http.Headers;
 import com.artipie.http.hm.RsHasHeaders;
+import com.artipie.http.rs.Header;
+import com.artipie.http.rs.RsWithHeaders;
 import com.artipie.http.rs.StandardRs;
 import io.reactivex.Flowable;
-import org.cactoos.map.MapEntry;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
@@ -45,7 +46,25 @@ class SliceWithHeadersTest {
             new SliceWithHeaders(
                 new SliceSimple(StandardRs.EMPTY), new Headers.From(header, value)
             ).response("GET /some/text HTTP/1.1", Headers.EMPTY, Flowable.empty()),
-            new RsHasHeaders(new MapEntry<>(header, value))
+            new RsHasHeaders(new Header(header, value))
+        );
+    }
+
+    @Test
+    void addsHeaderToAlreadyExistingHeaders() {
+        final String hone = "Keep-alive";
+        final String vone = "true";
+        final String htwo = "Authorization";
+        final String vtwo = "123";
+        MatcherAssert.assertThat(
+            new SliceWithHeaders(
+                new SliceSimple(
+                    new RsWithHeaders(StandardRs.EMPTY, hone, vone)
+                ), new Headers.From(htwo, vtwo)
+            ).response("GET /any/text HTTP/1.1", Headers.EMPTY, Flowable.empty()),
+            new RsHasHeaders(
+                new Header(hone, vone), new Header(htwo, vtwo)
+            )
         );
     }
 
