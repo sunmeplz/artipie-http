fc4f5abcbf77a63d3c98abce2ea4553c259e1717
Victor Noël <victor.noel@crazydwarves.org>
2020-06-02 19:58:45 +0200

    (#127) - Keep backward compatibility

--- src/main/java/com/artipie/http/rt/SliceRoute.java
+++ src/main/java/com/artipie/http/rt/SliceRoute.java
@@ -40,7 +40,7 @@ import org.reactivestreams.Publisher;
  * Routing slice.
  * <p>
  * {@link Slice} implementation which redirect requests to {@link Slice}
- * in {@link SliceRoute.Path} if {@link RtRule} matched.<br/>
+ * in {@link Path} if {@link RtRule} matched.<br/>
  * Usage:
  * <pre><code>
  * new SliceRoute(
@@ -60,13 +60,13 @@ public final class SliceRoute implements Slice {
     /**
      * Routes.
      */
-    private final List<Path> routes;
+    private final List<RtPath> routes;
 
     /**
      * New slice route.
      * @param routes Routes
      */
-    public SliceRoute(final Path... routes) {
+    public SliceRoute(final RtPath... routes) {
         this(new ListOf<>(routes));
     }
 
@@ -74,7 +74,7 @@ public final class SliceRoute implements Slice {
      * New slice route.
      * @param routes Routes
      */
-    public SliceRoute(final List<Path> routes) {
+    public SliceRoute(final List<RtPath> routes) {
         this.routes = routes;
     }
 
@@ -97,20 +97,38 @@ public final class SliceRoute implements Slice {
 
     /**
      * Route path.
-     * @since 0.10
+     * <p>
+     * A path to slice with routing rule. If
+     * {@link RtRule} passed, then the request will be redirected to
+     * underlying {@link Slice}.
+     * </p>
+     * @since 0.5
+     * @deprecated Use {@link RtRulePath} instead
      */
-    public interface Path {
+    @Deprecated
+    public static final class Path implements RtPath {
+
         /**
-         * Try respond.
-         * @param line Request line
-         * @param headers Headers
-         * @param body Body
-         * @return Response if passed routing rule
+         * Wrapped.
          */
-        Optional<Response> response(
-            String line,
-            Iterable<Map.Entry<String, String>> headers,
-            Publisher<ByteBuffer> body
-        );
+        private final RtPath wrapped;
+
+        /**
+         * New routing path.
+         * @param rule Rules to apply
+         * @param slice Slice to call
+         */
+        public Path(final RtRule rule, final Slice slice) {
+            this.wrapped = new RtRulePath(rule, slice);
+        }
+
+        @Override
+        public Optional<Response> response(
+            final String line,
+            final Iterable<Map.Entry<String, String>> headers,
+            final Publisher<ByteBuffer> body
+        ) {
+            return this.wrapped.response(line, headers, body);
+        }
     }
 }
