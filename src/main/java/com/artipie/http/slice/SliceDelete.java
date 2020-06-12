161467c8b3ed2d72892dfdca91fcef003659c0cf
paulodamaso <pauloeduardolobo@gmail.com>
2020-06-12 19:30:20 -0300

    For #148: added SliceDelete

---
+++ src/main/java/com/artipie/http/slice/SliceDelete.java
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.artipie.http.slice;
+
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.http.Response;
+import com.artipie.http.Slice;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import org.reactivestreams.Publisher;
+
+/**
+ * Delete decorator for Slice.
+ *
+ * @since 0.10
+ * @todo #138:30min Implement SliceDelete
+ *  Implement SliceDelete, which removes a key from storage. After that, enable
+ *  the tests in SliceDeleteTest and put back the coverage missedclass metric to
+ *  15 in pom.xml.
+ */
+@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+public final class SliceDelete implements Slice {
+
+    /**
+     * Storage.
+     */
+    private final Storage storage;
+
+    /**
+     * Key to delete.
+     */
+    private final Key key;
+
+    /**
+     * Constructor.
+     * @param storage Storage.
+     * @param key Key to delete.
+     */
+    public SliceDelete(final Storage storage, final Key key) {
+        this.storage = storage;
+        this.key = key;
+    }
+
+    @Override
+    public Response response(
+        final String line,
+        final Iterable<Map.Entry<String, String>> headers,
+        final Publisher<ByteBuffer> body) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/com/artipie/http/slice/SliceDeleteTest.java
@@ -0,0 +1,117 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.http.slice;
+
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.memory.InMemoryStorage;
+import com.artipie.http.rq.RequestLine;
+import io.reactivex.Flowable;
+import java.util.Collections;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link SliceDelete}.
+ *
+ * @since 0.10
+ */
+public class SliceDeleteTest {
+
+    /**
+     * Deleted key.
+     */
+    private static final String KEY = "deletedKey";
+
+    /**
+     * Delete method.
+     */
+    private static final String METHOD = "DELETE";
+
+    /**
+     * HTTP.
+     */
+    private static final String HTTP = "HTTP/1.1";
+
+    @Test
+    @Disabled
+    void deleteCorrectEntry() {
+        final Storage storage = new InMemoryStorage();
+        storage.save(
+            new Key.From(SliceDeleteTest.KEY),
+            new Content.From("deleted content".getBytes())
+        ).join();
+        new SliceDelete(
+            storage,
+            new Key.From(SliceDeleteTest.KEY)
+        ).response(
+            new RequestLine(
+                SliceDeleteTest.METHOD,
+                SliceDeleteTest.KEY,
+                SliceDeleteTest.HTTP
+            ).toString(),
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(
+            storage.exists(new Key.From(SliceDeleteTest.KEY)),
+            new IsEqual<>(false)
+        );
+    }
+
+    @Test
+    @Disabled
+    void doNotDeleteOtherEntry() {
+        final Storage storage = new InMemoryStorage();
+        final Key preserved = new Key.From("preservedKey");
+        storage.save(
+            new Key.From(SliceDeleteTest.KEY),
+            new Content.From("any content".getBytes())
+        ).join();
+        storage.save(
+            preserved,
+            new Content.From("preserved content".getBytes())
+        ).join();
+        new SliceDelete(
+            storage,
+            new Key.From(SliceDeleteTest.KEY)
+        ).response(
+            new RequestLine(
+                SliceDeleteTest.METHOD,
+                SliceDeleteTest.KEY,
+                SliceDeleteTest.HTTP
+            ).toString(),
+            Collections.emptyList(),
+            Flowable.empty()
+        );
+        MatcherAssert.assertThat(
+            storage.exists(preserved),
+            new IsEqual<>(true)
+        );
+    }
+}
+
