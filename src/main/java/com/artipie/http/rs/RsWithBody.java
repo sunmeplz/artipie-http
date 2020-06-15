69db8e576dddff417b8beb38e7bddef37a684d2c
Kirill <g4s8.public@gmail.com>
2020-06-15 15:53:59 +0300

    #176 - response content-size headers

--- src/main/java/com/artipie/http/rs/RsWithBody.java
+++ src/main/java/com/artipie/http/rs/RsWithBody.java
@@ -24,12 +24,14 @@
 
 package com.artipie.http.rs;
 
+import com.artipie.asto.Content;
 import com.artipie.http.Connection;
 import com.artipie.http.Headers;
 import com.artipie.http.Response;
 import io.reactivex.Flowable;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import org.reactivestreams.Publisher;
 
@@ -45,9 +47,9 @@ public final class RsWithBody implements Response {
     private final Response origin;
 
     /**
-     * Body publisher.
+     * Body content.
      */
-    private final Publisher<ByteBuffer> body;
+    private final Content body;
 
     /**
      * Decorates response with new text body.
@@ -82,7 +84,7 @@ public final class RsWithBody implements Response {
      * @param buf Body buffer
      */
     public RsWithBody(final Response origin, final ByteBuffer buf) {
-        this(origin, Flowable.just(buf));
+        this(origin, new Content.From(Optional.of((long) buf.remaining()), Flowable.just(buf)));
     }
 
     /**
@@ -94,18 +96,47 @@ public final class RsWithBody implements Response {
     }
 
     /**
-     * Decorates origin response body with publisher.
-     * @param origin Response
+     * Response with body from publisher.
+     * @param origin Origin response
      * @param body Publisher
      */
     public RsWithBody(final Response origin, final Publisher<ByteBuffer> body) {
+        this(origin, new Content.From(body));
+    }
+
+    /**
+     * Decorates origin response body with content.
+     * @param origin Response
+     * @param body Content
+     */
+    public RsWithBody(final Response origin, final Content body) {
         this.origin = origin;
         this.body = body;
     }
 
     @Override
     public CompletionStage<Void> send(final Connection con) {
-        return this.origin.send(new RsWithBody.ConWithBody(con, this.body));
+        return this.origin.send(
+            new RsWithBody.ConWithBody(
+                this.body.size()
+                    .map(size -> RsWithBody.withSize(con, size))
+                    .orElse(con),
+                this.body
+            )
+        );
+    }
+
+    /**
+     * Connection with {@code Content-Length} size header.
+     * @param con Origin connection
+     * @param size Content size
+     * @return Wrapped connection
+     */
+    private static Connection withSize(final Connection con, final long size) {
+        return new RsWithHeaders.ConWithHeaders(
+            con,
+            new Headers.From(new ContentLength(String.valueOf(size)))
+        );
     }
 
     /**
