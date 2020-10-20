ecaff1a3e143171c96213e465a2c30aa54c5c69f
olenagerasimova <olena.gerasimova@gmail.com>
2020-10-20 10:39:12 +0300

    #253 - added header value regex to RtRule.ByHeader

--- src/test/java/com/artipie/http/rt/RtRuleByHeaderTest.java
+++ src/test/java/com/artipie/http/rt/RtRuleByHeaderTest.java
@@ -24,6 +24,7 @@
 package com.artipie.http.rt;
 
 import com.artipie.http.Headers;
+import java.util.regex.Pattern;
 import org.cactoos.map.MapEntry;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
@@ -50,6 +51,28 @@ class RtRuleByHeaderTest {
     void falseIfHeaderIsNotPresent() {
         MatcherAssert.assertThat(
             new RtRule.ByHeader("my header").apply("rq line", Headers.EMPTY),
+            new IsEqual<>(false)
+        );
+    }
+
+    @Test
+    void trueIfHeaderIsPresentAndValueMatchesRegex() {
+        final String name = "content-type";
+        MatcherAssert.assertThat(
+            new RtRule.ByHeader(name, Pattern.compile("text/html.*")).apply(
+                "/some/path", new Headers.From(new MapEntry<>(name, "text/html; charset=utf-8"))
+            ),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    void falseIfHeaderIsPresentAndValueDoesNotMatchesRegex() {
+        final String name = "Accept-Encoding";
+        MatcherAssert.assertThat(
+            new RtRule.ByHeader(name, Pattern.compile("gzip.*")).apply(
+                "/another/path", new Headers.From(new MapEntry<>(name, "deflate"))
+            ),
             new IsEqual<>(false)
         );
     }
