01ac8efe73677dde7b5808978ee1d32305a27dcb
Kirill <g4s8.public@gmail.com>
2021-06-09 09:58:24 +0300

    Multipart request part implementation
    For #299 - implemented single part request parsing:
     - provides `push` method to receive byte-buffer chunks
     - build body part headers from chunks until delimiter
     - notify upstream on ready (headers are built)
     - publish next chunks after headers to downstream

---
+++ src/main/java/com/artipie/http/rq/multipart/MultiPart.java
@@ -0,0 +1,206 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/npm-adapter/LICENSE.txt
+ */
+package com.artipie.http.rq.multipart;
+
+import com.artipie.http.Headers;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+/**
+ * Multipart request part.
+ * @since 1.0
+ */
+final class MultiPart implements RqMultipart.Part, ByteBufferTokenizer.Receiver, Subscription {
+
+    /**
+     * Header buffer capacity.
+     */
+    private static final int CAP_HEADER = 256;
+
+    /**
+     * Part body buffer capacity.
+     */
+    private static final int CAP_PART = 1024;
+
+    /**
+     * Delimiter token.
+     */
+    private static final String DELIM = "\r\n\r\n";
+
+    /**
+     * Header state flag.
+     */
+    private static final int STATE_HEADER = 1;
+
+    /**
+     * Body state flag.
+     */
+    private static final int STATE_BODY = 2;
+
+    /**
+     * Upstream subscription.
+     */
+    private final Subscription upstream;
+
+    /**
+     * CRLF tokenizer.
+     */
+    private final ByteBufferTokenizer tokenizer;
+
+    /**
+     * Multipart header.
+     */
+    private final MultipartHeaders hdr;
+
+    /**
+     * Downstream reference.
+     */
+    private final AtomicReference<Subscriber<? super ByteBuffer>> downstream;
+
+    /**
+     * Current state.
+     */
+    private final AtomicInteger state;
+
+    /**
+     * Ready callback.
+     * <p>
+     * Called when the header is received and the part could be submitted to
+     * {@link com.artipie.http.rq.multipart.RqMultipart.Part} downstream.
+     * </p>
+     */
+    private final Consumer<? super RqMultipart.Part> ready;
+
+    /**
+     * Temporary body accumulator.
+     * <p>
+     * It's needed when the downstream connected after the part of body received.
+     * It may happen if the first chunk of body received with last header chunk
+     * before downstream subscription.
+     * </p>
+     */
+    private final BufAccumulator tmpacc;
+
+    /**
+     * Async back-pressure executor.
+     */
+    private final ExecutorService exec;
+
+    /**
+     * Completed flag.
+     */
+    private final AtomicBoolean completed;
+
+    /**
+     * New multipart request part.
+     * @param upstream Upstream subscription
+     * @param ready Ready callback
+     * @param exec Back-pressure async executor
+     */
+    MultiPart(final Subscription upstream, final Consumer<? super RqMultipart.Part> ready,
+        final ExecutorService exec) {
+        this.upstream = upstream;
+        this.ready = ready;
+        this.exec = exec;
+        this.tokenizer = new ByteBufferTokenizer(
+            this, MultiPart.DELIM.getBytes(), MultiPart.CAP_PART
+        );
+        this.hdr = new MultipartHeaders(MultiPart.CAP_HEADER);
+        this.downstream = new AtomicReference<>();
+        this.state = new AtomicInteger(MultiPart.STATE_HEADER);
+        this.tmpacc = new BufAccumulator(MultiPart.CAP_HEADER);
+        this.completed = new AtomicBoolean();
+    }
+
+    @Override
+    public Headers headers() {
+        return this.hdr;
+    }
+
+    @Override
+    public void subscribe(final Subscriber<? super ByteBuffer> sub) {
+        synchronized (this.downstream) {
+            if (this.downstream.get() != null) {
+                sub.onSubscribe(DummySubscription.VALUE);
+                sub.onError(new IllegalStateException("Downstream already connected"));
+                return;
+            }
+            sub.onSubscribe(this);
+            final ByteBuffer acc = this.tmpacc.duplicate();
+            acc.rewind();
+            if (acc.hasRemaining()) {
+                sub.onNext(acc);
+            }
+            this.tmpacc.close();
+            if (this.completed.get()) {
+                sub.onComplete();
+            } else {
+                this.downstream.set(sub);
+            }
+        }
+    }
+
+    @Override
+    public void receive(final ByteBuffer next, final boolean end) {
+        synchronized (this.downstream) {
+            final int current = this.state.get();
+            if (current == MultiPart.STATE_HEADER) {
+                this.hdr.push(next);
+                this.upstream.request(1);
+                if (end) {
+                    this.state.incrementAndGet();
+                    this.ready.accept(this);
+                }
+            } else if (current == MultiPart.STATE_BODY) {
+                final Subscriber<? super ByteBuffer> subscriber = this.downstream.get();
+                if (subscriber == null) {
+                    this.tmpacc.push(next);
+                } else {
+                    subscriber.onNext(next);
+                }
+            }
+            if (end && current == MultiPart.STATE_BODY
+                && this.completed.compareAndSet(false, true)) {
+                final Subscriber<? super ByteBuffer> sub = this.downstream.getAndSet(null);
+                if (sub != null) {
+                    sub.onComplete();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void request(final long amt) {
+        this.exec.submit(() -> this.upstream.request(amt));
+    }
+
+    @Override
+    public void cancel() {
+        this.downstream.set(null);
+        this.upstream.cancel();
+    }
+
+    /**
+     * Push next chunk of raw data.
+     * @param chunk Chunk buffer
+     */
+    void push(final ByteBuffer chunk) {
+        this.tokenizer.push(chunk);
+    }
+
+    /**
+     * Flush all data in temporary buffers.
+     */
+    void flush() {
+        this.push(ByteBuffer.wrap(MultiPart.DELIM.getBytes(StandardCharsets.US_ASCII)));
+    }
+}
---
+++ src/test/java/com/artipie/http/rq/multipart/MultiPartTest.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/npm-adapter/LICENSE.txt
+ */
+package com.artipie.http.rq.multipart;
+
+import com.artipie.asto.ext.PublisherAs;
+import com.artipie.http.hm.RqHasHeader;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableSubscriber;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+
+/**
+ * Test case for {@link MultiPart}.
+ * @since 1.0
+ */
+final class MultiPartTest {
+
+    @Test
+    void parsePart() throws Exception {
+        final Flowable<ByteBuffer> upstream = Flowable.fromArray(
+            "Content-l", "ength", ": 24\r\n",
+            "Con", "tent-typ", "e: ", "appl", "ication/jso", "n\r\n\r\n{\"foo",
+            "\": \"b", "ar\", ", "\"val\": [4]}\r\n"
+        ).map(str -> ByteBuffer.wrap(str.getBytes()));
+        final CompletableFuture<RqMultipart.Part> future = new CompletableFuture<>();
+        upstream.subscribe(new Subscriber(future));
+        final RqMultipart.Part part = future.get();
+        MatcherAssert.assertThat(
+            part.headers(),
+            Matchers.allOf(
+                new RqHasHeader("content-length", Matchers.contains("24")),
+                new RqHasHeader("content-type", Matchers.contains("application/json"))
+            )
+        );
+        MatcherAssert.assertThat(
+            new PublisherAs(part).string(StandardCharsets.US_ASCII).toCompletableFuture().get(),
+            Matchers.equalTo("{\"foo\": \"bar\", \"val\": [4]}")
+        );
+    }
+
+    /**
+     * Part subscriber.
+     * @since 1.0
+     */
+    private static final class Subscriber implements FlowableSubscriber<ByteBuffer> {
+
+        /**
+         * Part future.
+         */
+        private final CompletableFuture<RqMultipart.Part> future;
+
+        /**
+         * Current part.
+         */
+        private volatile MultiPart part;
+
+        /**
+         * New subscriber.
+         * @param future Part future
+         */
+        Subscriber(final CompletableFuture<RqMultipart.Part> future) {
+            this.future = future;
+        }
+
+        @Override
+        public void onSubscribe(final Subscription subscription) {
+            synchronized (this.future) {
+                if (this.part != null) {
+                    this.future.completeExceptionally(
+                        new IllegalStateException("Subscribed twice")
+                    );
+                    return;
+                }
+                this.part = new MultiPart(
+                    subscription, this.future::complete, Executors.newCachedThreadPool()
+                );
+                subscription.request(1);
+            }
+        }
+
+        @Override
+        public void onNext(final ByteBuffer buffer) {
+            this.part.push(buffer);
+        }
+
+        @Override
+        public void onError(final Throwable err) {
+            this.future.completeExceptionally(err);
+        }
+
+        @Override
+        public void onComplete() {
+            this.part.flush();
+        }
+    }
+}
