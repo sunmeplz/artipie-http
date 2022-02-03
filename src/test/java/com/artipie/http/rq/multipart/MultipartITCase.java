31a92fdd7b038baf295cefb25b8194fb689acbed
Kirill <g4s8.public@gmail.com>
2022-02-03 16:19:50 +0300

    test(multipart): multipart - storage integratin (#433)
    Add integration test to verify multipart with
    `FileStorage` integration. It performs upload
    of big-enough binary data, parse this data from
    multipart and save it to `FileStorage`, then
    verify file's content.
    
    Ref: #420

--- src/test/java/com/artipie/http/rq/multipart/MultipartITCase.java
+++ src/test/java/com/artipie/http/rq/multipart/MultipartITCase.java
@@ -4,7 +4,10 @@
  */
 package com.artipie.http.rq.multipart;
 
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
 import com.artipie.asto.ext.PublisherAs;
+import com.artipie.asto.fs.FileStorage;
 import com.artipie.http.Headers;
 import com.artipie.http.Response;
 import com.artipie.http.Slice;
@@ -15,10 +18,14 @@ import com.artipie.http.rs.RsWithBody;
 import com.artipie.http.rs.RsWithStatus;
 import com.artipie.http.rs.StandardRs;
 import com.artipie.vertx.VertxSliceServer;
+import hu.akarnokd.rxjava2.interop.SingleInterop;
 import io.reactivex.Flowable;
+import io.reactivex.Single;
 import io.vertx.reactivex.core.Vertx;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicReference;
@@ -27,17 +34,21 @@ import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.reactivestreams.Publisher;
 
 /**
  * Integration tests for multipart feature.
  * @since 1.2
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @checkstyle MagicNumberCheck (500 line)
+ * @checkstyle MagicNumberCheck (500 line)
  */
 final class MultipartITCase {
 
@@ -116,7 +127,6 @@ final class MultipartITCase {
             );
             try (CloseableHttpResponse rsp = cli.execute(post)) {
                 MatcherAssert.assertThat(
-                    // @checkstyle MagicNumberCheck (1 line)
                     "code should be 200", rsp.getCode(), Matchers.equalTo(200)
                 );
             }
@@ -154,12 +164,7 @@ final class MultipartITCase {
                 )
             )
         );
-        // @checkstyle MagicNumberCheck (1 line)
-        final byte[] buf = new byte[2048 * 17];
-        final byte[] chunk = "0123456789ABCDEF\n".getBytes(StandardCharsets.US_ASCII);
-        for (int pos = 0; pos < buf.length; pos += chunk.length) {
-            System.arraycopy(chunk, 0, buf, pos, chunk.length);
-        }
+        final byte[] buf = testData(2048 * 17);
         try (CloseableHttpClient cli = HttpClients.createDefault()) {
             final HttpPost post = new HttpPost(String.format("http://localhost:%d/", this.port));
             post.setEntity(
@@ -171,7 +176,6 @@ final class MultipartITCase {
             );
             try (CloseableHttpResponse rsp = cli.execute(post)) {
                 MatcherAssert.assertThat(
-                    // @checkstyle MagicNumberCheck (1 line)
                     "code should be 200", rsp.getCode(), Matchers.equalTo(200)
                 );
             }
@@ -181,6 +185,74 @@ final class MultipartITCase {
             result.get(),
             Matchers.equalTo(new String(buf, StandardCharsets.US_ASCII))
         );
+    }
+
+    @Test
+    @SuppressWarnings("PMD.AvoidDuplicateLiterals")
+    void saveMultipartToFile(@TempDir final Path path) throws Exception {
+        this.container.deploy(
+            (line, headers, body) -> new AsyncResponse(
+                Flowable.fromPublisher(
+                    new RqMultipart(new Headers.From(headers), body).inspect(
+                        (part, sink) -> {
+                            final ContentDisposition cds =
+                                new ContentDisposition(part.headers());
+                            if (cds.fieldName().equals("content")) {
+                                sink.accept(part);
+                            } else {
+                                sink.ignore(part);
+                            }
+                            final CompletableFuture<Void> res = new CompletableFuture<>();
+                            res.complete(null);
+                            return res;
+                        }
+                    )
+                ).flatMapSingle(
+                    part -> Single.fromFuture(
+                        new FileStorage(path).save(
+                            new Key.From(new ContentDisposition(part.headers()).fileName()),
+                            new Content.From(part)
+                        ).thenApply(none -> 0)
+                    )
+                ).toList().to(SingleInterop.get()).thenApply(none -> StandardRs.OK)
+            )
+        );
+        final byte[] buf = testData(2048 * 17);
+        final String filename = "data.bin";
+        try (CloseableHttpClient cli = HttpClients.createDefault()) {
+            final HttpPost post = new HttpPost(String.format("http://localhost:%d/", this.port));
+            post.setEntity(
+                MultipartEntityBuilder.create()
+                    .addTextBody("name", "test-data")
+                    .addBinaryBody("content", buf, ContentType.APPLICATION_OCTET_STREAM, filename)
+                    .addTextBody("foo", "bar")
+                    .build()
+            );
+            try (CloseableHttpResponse rsp = cli.execute(post)) {
+                MatcherAssert.assertThat(
+                    "code should be 200", rsp.getCode(), Matchers.equalTo(200)
+                );
+            }
+        }
+        MatcherAssert.assertThat(
+            "content data should be save correctly",
+            Files.readAllBytes(path.resolve(filename)),
+            Matchers.equalTo(buf)
+        );
+    }
+
+    /**
+     * Create new test data buffer for payload.
+     * @param size Buffer size
+     * @return Byte array
+     */
+    private static byte[] testData(final int size) {
+        final byte[] buf = new byte[size];
+        final byte[] chunk = "0123456789ABCDEF\n".getBytes(StandardCharsets.US_ASCII);
+        for (int pos = 0; pos < buf.length; pos += chunk.length) {
+            System.arraycopy(chunk, 0, buf, pos, chunk.length);
+        }
+        return buf;
     }
 
     /**
