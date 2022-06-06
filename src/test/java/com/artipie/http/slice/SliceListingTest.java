5e8e8f165bd4490f27c8cfc24eed0f3d1f7686e3
Alexey Borodin <swizbizy@gmail.com>
2022-06-06 16:19:21 +0300

    test: extract Storage to field
    extract Storage to private field and make setUp method for populate

--- src/test/java/com/artipie/http/slice/SliceListingTest.java
+++ src/test/java/com/artipie/http/slice/SliceListingTest.java
@@ -18,28 +18,36 @@ import java.util.Collections;
 import javax.json.Json;
 import org.cactoos.map.MapEntry;
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * Test case for {@link SliceListingTest}.
- * @since 1.1.4
+ * @since 1.2
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class SliceListingTest {
+    /**
+     * Storage.
+     */
+    private final Storage storage = new InMemoryStorage();
+
+    @BeforeEach
+    void setUp() {
+        this.storage.save(new Key.From("one/target1.txt"), new Content.Empty()).join();
+        this.storage.save(new Key.From("one/two/target2.txt"), new Content.Empty()).join();
+    }
 
     @ParameterizedTest
     @CsvSource({
-        "not-exists/, ''",
-        "one/, 'one/target1.txt\none/two/target2.txt'"
+        "not-exists/,''",
+        "one/,'one/target1.txt\none/two/target2.txt'"
     })
     void responseTextType(final String path, final String body) {
-        final Storage storage = new InMemoryStorage();
-        storage.save(new Key.From("one/target1.txt"), new Content.Empty()).join();
-        storage.save(new Key.From("one/two/target2.txt"), new Content.Empty()).join();
         MatcherAssert.assertThat(
-            new SliceListing(storage, "text/plain", ListingFormat.Standard.TEXT).response(
+            new SliceListing(this.storage, "text/plain", ListingFormat.Standard.TEXT).response(
                 get(path), Collections.emptyList(), Flowable.empty()
             ),
             new ResponseMatcher(
@@ -58,14 +66,11 @@ class SliceListingTest {
 
     @Test
     void responseJsonType() {
-        final String one = "one/example1.txt";
-        final String two = "one/two/example2.txt";
-        final Storage storage = new InMemoryStorage();
-        storage.save(new Key.From(one), new Content.Empty()).join();
-        storage.save(new Key.From(two), new Content.Empty()).join();
-        final String json = Json.createArrayBuilder(Arrays.asList(one, two)).build().toString();
+        final String json = Json.createArrayBuilder(
+            Arrays.asList("one/target1.txt", "one/two/target2.txt")
+        ).build().toString();
         MatcherAssert.assertThat(
-            new SliceListing(storage, "application/json", ListingFormat.Standard.JSON)
+            new SliceListing(this.storage, "application/json", ListingFormat.Standard.JSON)
                 .response(get("one/"), Collections.emptyList(), Flowable.empty()),
             new ResponseMatcher(
                 RsStatus.OK,
